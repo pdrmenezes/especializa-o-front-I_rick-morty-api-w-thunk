cddbe7a382dafc0181f48b1ed9dfed74833bd546
pdrmenezes <pdrmenezes1@gmail.com>
2023-02-26 00:32:49 -0300

    adding vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/especializacao-front-I_rick-morty-api-w-redux-thunk/',
   plugins: [react()]
 })
